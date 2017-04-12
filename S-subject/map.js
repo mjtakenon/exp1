0e27bbbd6132d0864f968b71feb13a2561187106
mjtakenon <s12528@salesio-sp.ac.jp>
2017-04-12 12:39:07 +0900

    Update map.js

--- S-subject/map.js
+++ S-subject/map.js
@@ -12,3 +12,9 @@ var mapOptions = {
 
 // [canvas]に、[mapOptions]の内容の、地図のインスタンス([map])を作成する
 var map = new google.maps.Map( canvas, mapOptions ) ;
+
+// [marker]の設定
+var marker = new google.maps.Marker( {
+	map: map ,	// 地図
+	position: new google.maps.LatLng( 34.7192369,137.7144583 )	// 位置座標
+} ) ;
